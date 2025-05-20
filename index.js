cc44e9a1465445c206d85228e041aba7cb967ac0
naruya izumi <122040630+naruyaizumi@users.noreply.github.com>
2025-05-20 15:40:09 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,46 +1,78 @@
 import { GlobalFonts } from '@napi-rs/canvas'
 import path from 'path'
 import { fileURLToPath } from 'url'
-const pkg = await import('./package.json', { assert: { type: 'json' } })
+import fs from 'fs/promises'
 
+const pkg = JSON.parse(await fs.readFile(new URL('./package.json', import.meta.url)))
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Poppins/Poppins-Regular.ttf`, "Poppins")
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Poppins/Poppins-Bold.ttf`, "Poppins Bold")
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Manrope/Manrope-Regular.ttf`, "Manrope")
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Manrope/Manrope-Bold.ttf`, "Manrope Bold")
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Others/AbyssinicaSIL-Regular.ttf`, "Abyss")
 GlobalFonts.registerFromPath(`${__dirname}/assets/fonts/Others/ChirpRegular.ttf`, "Chirp")
+
+import Affect from './src/affect.js'
+import Batslap from './src/batslap.js'
+import Beautiful from './src/beautiful.js'
+import Darkness from './src/darkness.js'
+import Delete from './src/delete.js'
+import Gay from './src/gay.js'
+import Greyscale from './src/greyscale.js'
+import Invert from './src/invert.js'
+import Kiss from './src/kiss.js'
+import OldRank from './src/oldrank.js'
+import Rank from './src/rank.js'
+import OldSpotify from './src/oldspotify.js'
+import Spotify from './src/spotify.js'
+import WelcomeLeave from './src/welcome-leave.js'
+import Ship from './src/ship.js'
+import Top from './src/top.js'
+import LevelUp from './src/level-up.js'
+import Profile from './src/profile.js'
+import Security from './src/security.js'
+import Captcha from './src/captcha.js'
+import Tweet from './src/tweet.js'
+import Instagram from './src/instagram.js'
+import Util from './plugins/Util.js'
+
 const { emitWarning } = process
 process.emitWarning = (warning, ...args) => {
 if (args[0] === 'ExperimentalWarning') return
 if (args[0]?.type === 'ExperimentalWarning') return
 emitWarning(warning, ...args)
 }
+
 export const Image = {
-affect: (await import('./src/affect.js')).default,
-batslap: (await import('./src/batslap.js')).default,
-beautiful: (await import('./src/beautiful.js')).default,
-darkness: (await import('./src/darkness.js')).default,
-delete: (await import('./src/delete.js')).default,
-gay: (await import('./src/gay.js')).default,
-greyscale: (await import('./src/greyscale.js')).default,
-invert: (await import('./src/invert.js')).default,
-kiss: (await import('./src/kiss.js')).default
+affect: Affect,
+batslap: Batslap,
+beautiful: Beautiful,
+darkness: Darkness,
+delete: Delete,
+gay: Gay,
+greyscale: Greyscale,
+invert: Invert,
+kiss: Kiss
 }
-export const oldRank = (await import('./src/oldrank.js')).default
-export const Rank = (await import('./src/rank.js')).default
-export const oldSpotify = (await import('./src/oldspotify.js')).default
-export const Spotify = (await import('./src/spotify.js')).default
-export const Util = (await import('./plugins/Util.js')).default
-export const WelcomeLeave = (await import('./src/welcome-leave.js')).default
-export const Ship = (await import('./src/ship.js')).default
-export const Top = (await import('./src/top.js')).default
-export const LevelUp = (await import('./src/level-up.js')).default
-export const Profile = (await import('./src/profile.js')).default
-export const Security = (await import('./src/security.js')).default
-export const Captcha = (await import('./src/captcha.js')).default
-export const Tweet = (await import('./src/tweet.js')).default
-export const Instagram = (await import('./src/instagram.js')).default
+
+export {
+OldRank,
+Rank,
+OldSpotify,
+Spotify,
+Util,
+WelcomeLeave,
+Ship,
+Top,
+LevelUp,
+Profile,
+Security,
+Captcha,
+Tweet,
+Instagram
+}
+
 export const author = "Naruya-Izumi"
-export const version = pkg.default.version
+export const version = pkg.version
